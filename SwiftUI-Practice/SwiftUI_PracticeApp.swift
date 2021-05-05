a7e47e8a5753f562c2a7df5b185ca171ee3cff91
H43RO <h2is1234@naver.com>
2021-05-06 03:21:35 +0900

    Initial

---
+++ SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by H43RO on 2021/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
