6a13bf17746c89b72670ac8e942b0e602f689115
H43RO <h2is1234@naver.com>
2021-05-06 04:21:39 +0900

    Detail View Data Binding

---
+++ SwiftUI-Practice/Views/LandmarkDetail.swift
@@ -0,0 +1,56 @@
+//
+//  LandmarkDetail.swift
+//  SwiftUI-Practice
+//
+//  Created by H43RO on 2021/05/06.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        ScrollView{
+            VStack{
+                MapView(coordinate: landmark.locationCoordinate)
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack(alignment: .leading) {
+                    Text(landmark.name)
+                        .font(.title)
+                        .fontWeight(.bold)
+                    HStack{
+                        Text(landmark.park)
+                            .font(.subheadline)
+                        Spacer()
+                        Text(landmark.state)
+                            .font(.subheadline)
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    
+                    Divider()
+                    
+                    Text(landmark.description)
+                }
+                .padding()
+                
+                Spacer()
+            }
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
